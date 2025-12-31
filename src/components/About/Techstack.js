f71c80857b61ace568b9d3477c6c1ab264f0930b
Umesh <globaltechumesh11@gmail.com>
2025-12-31 17:34:21 +0545

    New Methgfdgdfgdfgdfgdfgdfgdfg

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,37 +1,21 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { CgCPlusPlus } from "react-icons/cg";
-import {
-  DiNodejs,
-  DiMongodb,
-  DiGit,
-} from "react-icons/di";
+import { DiGit } from "react-icons/di";
 import {
   SiFirebase,
   SiFlutter,
   SiDart,
   SiMicrosoftsqlserver
 } from "react-icons/si";
-import { TbBrandGolang } from "react-icons/tb";
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-       <Col xs={4} md={2} className="tech-icons">
-        <SiFlutter />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiDart />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiFirebase />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiMicrosoftsqlserver />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col>
+      <Col xs={4} md={2} className="tech-icons"><SiFlutter /></Col>
+      <Col xs={4} md={2} className="tech-icons"><SiDart /></Col>
+      <Col xs={4} md={2} className="tech-icons"><SiFirebase /></Col>
+      <Col xs={4} md={2} className="tech-icons"><SiMicrosoftsqlserver /></Col>
+      <Col xs={4} md={2} className="tech-icons"><DiGit /></Col>
     </Row>
   );
 }
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -3,8 +3,6 @@ import { Col, Row } from "react-bootstrap";
 import {
   SiVisualstudiocode,
   SiPostman,
-  SiSlack,
-  SiVercel,
   SiMacos,
   SiAndroidstudio
 } from "react-icons/si";
@@ -12,18 +10,10 @@ import {
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiMacos />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVisualstudiocode />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostman />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiAndroidstudio />
-      </Col>
+      <Col xs={4} md={2} className="tech-icons"><SiMacos /></Col>
+      <Col xs={4} md={2} className="tech-icons"><SiVisualstudiocode /></Col>
+      <Col xs={4} md={2} className="tech-icons"><SiPostman /></Col>
+      <Col xs={4} md={2} className="tech-icons"><SiAndroidstudio /></Col>
     </Row>
   );
 }
